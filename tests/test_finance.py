3ce3a24f3d4ee59fe401d1cb7b1a60b8c4689994
Jeffrey Molendijk <jeffreymolendijk@live.com>
2023-01-05 11:19:49 +1100

    black autoformatting

--- tests/test_finance.py
+++ tests/test_finance.py
@@ -3,31 +3,54 @@ import pandas as pd
 import plotly
 import os
 
+
 def test_read_File():
-    assert isinstance(read_File(os.path.join('data', 'savings.xlsx')), pd.DataFrame), 'Is not a dataframe'
+    assert isinstance(
+        read_File(os.path.join("data", "savings.xlsx")), pd.DataFrame
+    ), "Is not a dataframe"
+
 
 def test_finance():
-    portfolio = finance(savings=os.path.join('data', 'savings.xlsx'),
-                        stocks=os.path.join('data', 'stocks.xlsx'),
-                        superannuation=os.path.join('data', 'super.xlsx'))
-    assert isinstance(portfolio.savings, pd.DataFrame), 'savings is not a dataframe'
-    assert isinstance(portfolio.stocks, pd.DataFrame), 'stocks is not a dataframe'
-    assert isinstance(portfolio.superannuation, pd.DataFrame), 'savings is not a dataframe'
+    portfolio = finance(
+        savings=os.path.join("data", "savings.xlsx"),
+        stocks=os.path.join("data", "stocks.xlsx"),
+        superannuation=os.path.join("data", "super.xlsx"),
+    )
+    assert isinstance(portfolio.savings, pd.DataFrame), "savings is not a dataframe"
+    assert isinstance(portfolio.stocks, pd.DataFrame), "stocks is not a dataframe"
+    assert isinstance(
+        portfolio.superannuation, pd.DataFrame
+    ), "savings is not a dataframe"
+
 
 def test_get_portfolio():
-    portfolio = finance(savings=os.path.join('data', 'savings.xlsx'),
-                        stocks=os.path.join('data', 'stocks.xlsx'),
-                        superannuation=os.path.join('data', 'super.xlsx'))
-    assert isinstance(portfolio.get_portfolio(), pd.DataFrame), 'get_portfolio() should return a dataframe'
+    portfolio = finance(
+        savings=os.path.join("data", "savings.xlsx"),
+        stocks=os.path.join("data", "stocks.xlsx"),
+        superannuation=os.path.join("data", "super.xlsx"),
+    )
+    assert isinstance(
+        portfolio.get_portfolio(), pd.DataFrame
+    ), "get_portfolio() should return a dataframe"
+
 
 def test_show_portfolio():
-    portfolio = finance(savings=os.path.join('data', 'savings.xlsx'),
-                        stocks=os.path.join('data', 'stocks.xlsx'),
-                        superannuation=os.path.join('data', 'super.xlsx'))
-    assert isinstance(portfolio.show_portfolio(), plotly.graph_objs._figure.Figure), 'show_portfolio() should return a plotly object'
+    portfolio = finance(
+        savings=os.path.join("data", "savings.xlsx"),
+        stocks=os.path.join("data", "stocks.xlsx"),
+        superannuation=os.path.join("data", "super.xlsx"),
+    )
+    assert isinstance(
+        portfolio.show_portfolio(), plotly.graph_objs._figure.Figure
+    ), "show_portfolio() should return a plotly object"
+
 
 def test_show_monthly_change():
-    portfolio = finance(savings=os.path.join('data', 'savings.xlsx'),
-                        stocks=os.path.join('data', 'stocks.xlsx'),
-                        superannuation=os.path.join('data', 'super.xlsx'))
-    assert isinstance(portfolio.show_monthly_change(), plotly.graph_objs._figure.Figure), 'show_monthly_change() should return a plotly object'
+    portfolio = finance(
+        savings=os.path.join("data", "savings.xlsx"),
+        stocks=os.path.join("data", "stocks.xlsx"),
+        superannuation=os.path.join("data", "super.xlsx"),
+    )
+    assert isinstance(
+        portfolio.show_monthly_change(), plotly.graph_objs._figure.Figure
+    ), "show_monthly_change() should return a plotly object"
