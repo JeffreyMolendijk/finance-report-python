75416b56674fa4c1bef5595bb59a9f5c979cdedd
Jeffrey Molendijk <jeffreymolendijk@live.com>
2023-01-05 11:18:15 +1100

    black autoformatting

--- finrep/finrep.py
+++ finrep/finrep.py
@@ -17,9 +17,9 @@ def read_File(file_path: str):
     """
     if isstring(file_path):
         try:
-            if file_path.endswith('.csv'):
+            if file_path.endswith(".csv"):
                 return pd.read_csv(file_path)
-            if file_path.endswith('.xlsx'):
+            if file_path.endswith(".xlsx"):
                 return pd.read_excel(file_path)
         except ValueError:
             return None
@@ -31,37 +31,45 @@ class finance:
 
     def __init__(self, savings: str, stocks: str, superannuation: str):
         df_savings = read_File(savings)
-        df_savings['type'] = 'savings'
+        df_savings["type"] = "savings"
         self.savings = df_savings
 
         df_superannuation = read_File(superannuation)
-        df_superannuation['type'] = 'superannuation'
+        df_superannuation["type"] = "superannuation"
         self.superannuation = df_superannuation
 
         df_stocks = read_File(stocks)
-        mindate = min(df_savings['date'].min(), df_superannuation['date'].min())
-        maxdate = max(df_savings['date'].max(), df_superannuation['date'].max())
-        idx = pd.date_range(mindate, maxdate, freq='MS')
-        idx_df = pd.DataFrame({'date': idx})
-        stocks_range = idx_df.merge(df_stocks, how='left')
-        stocks_range = stocks_range[['date', 'stock', 'units']]
-        tickers = stocks_range['stock'].dropna().unique()
-        outlist = list(product(stocks_range['date'].unique(), tickers))
-        stocks_range = pd.DataFrame(data=outlist, columns=['date', 'stock']).merge(
-            stocks_range, on=['date', 'stock'], how='left')
-        stocks_range['units'] = stocks_range['units'].fillna(0)
-        stocks_range['unitsum'] = stocks_range.groupby(['stock']).cumsum()
-        stocks_range = stocks_range.drop('units', axis=1)
-        hist = yf.download(list(tickers), start=mindate, end=maxdate, interval='1mo')
+        mindate = min(df_savings["date"].min(), df_superannuation["date"].min())
+        maxdate = max(df_savings["date"].max(), df_superannuation["date"].max())
+        idx = pd.date_range(mindate, maxdate, freq="MS")
+        idx_df = pd.DataFrame({"date": idx})
+        stocks_range = idx_df.merge(df_stocks, how="left")
+        stocks_range = stocks_range[["date", "stock", "units"]]
+        tickers = stocks_range["stock"].dropna().unique()
+        outlist = list(product(stocks_range["date"].unique(), tickers))
+        stocks_range = pd.DataFrame(data=outlist, columns=["date", "stock"]).merge(
+            stocks_range, on=["date", "stock"], how="left"
+        )
+        stocks_range["units"] = stocks_range["units"].fillna(0)
+        stocks_range["unitsum"] = stocks_range.groupby(["stock"]).cumsum()
+        stocks_range = stocks_range.drop("units", axis=1)
+        hist = yf.download(list(tickers), start=mindate, end=maxdate, interval="1mo")
 
         res = []
         for ticker in tickers:
-            res.append(pd.DataFrame({'date': hist.index,
-                                    'price': hist['Open'][ticker].values,
-                                    'stock': ticker}).reset_index(drop=True))
-        stocks_range = stocks_range.merge(pd.concat(res), on=['date', 'stock'], 
-                                            how='left')
-        stocks_range['value'] = stocks_range['unitsum'] * stocks_range['price']
+            res.append(
+                pd.DataFrame(
+                    {
+                        "date": hist.index,
+                        "price": hist["Open"][ticker].values,
+                        "stock": ticker,
+                    }
+                ).reset_index(drop=True)
+            )
+        stocks_range = stocks_range.merge(
+            pd.concat(res), on=["date", "stock"], how="left"
+        )
+        stocks_range["value"] = stocks_range["unitsum"] * stocks_range["price"]
         self.stocks = stocks_range
 
     def __validate_pandas(self):
@@ -70,10 +78,12 @@ class finance:
         Returns:
             _type_: boolean
         """
-        if len(self.savings) == 2 and \
-            len(self.superannuation) == 2 and \
-            len(self.stocks) == 4:
-            return True 
+        if (
+            len(self.savings) == 2
+            and len(self.superannuation) == 2
+            and len(self.stocks) == 4
+        ):
+            return True
         return False
 
     def get_portfolio(self):
@@ -82,8 +92,9 @@ class finance:
         Returns:
             _type_: pandas DataFrame
         """
-        stockval = self.stocks[['date', 'value', 'stock']].rename(columns={
-            'value': 'amount', 'stock': 'type'})
+        stockval = self.stocks[["date", "value", "stock"]].rename(
+            columns={"value": "amount", "stock": "type"}
+        )
         portfolio_total = pd.concat([self.savings, self.superannuation, stockval])
         return portfolio_total
 
@@ -93,8 +104,13 @@ class finance:
         Returns:
             _type_: plotly object
         """
-        fig = px.bar(self.get_portfolio(), x="date", y="amount", color="type", 
-                        title="Portfolio distribution")
+        fig = px.bar(
+            self.get_portfolio(),
+            x="date",
+            y="amount",
+            color="type",
+            title="Portfolio distribution",
+        )
         return fig
 
     def show_monthly_change(self):
@@ -103,13 +119,13 @@ class finance:
         Returns:
             _type_: plotly object
         """
-        x = self.get_portfolio().groupby('date').sum('amount')
-        x = x.diff().reset_index().iloc[2:,:]
+        x = self.get_portfolio().groupby("date").sum("amount")
+        x = x.diff().reset_index().iloc[2:, :]
         fig = px.bar(x, x="date", y="amount", title="Monthly change")
         return fig
 
     def forecast(self, months_predict: int):
-        print(f'In {months_predict} months you will have infinite money!')
+        print(f"In {months_predict} months you will have infinite money!")
 
     def optimize_portfolio():
         pass  # tweak contribution parameters to determine optimum
@@ -117,9 +133,11 @@ class finance:
 
 def main():
     """Contains example analysis code"""
-    portfolio = finance(savings=os.path.join('data', 'savings.xlsx'),
-                        stocks=os.path.join('data', 'stocks.xlsx'),
-                        superannuation=os.path.join('data', 'super.xlsx'))
+    portfolio = finance(
+        savings=os.path.join("data", "savings.xlsx"),
+        stocks=os.path.join("data", "stocks.xlsx"),
+        superannuation=os.path.join("data", "super.xlsx"),
+    )
 
     print(portfolio.get_portfolio())
     print(type(portfolio.show_portfolio()))
